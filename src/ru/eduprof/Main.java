b66d189e7d17ada4d9f512068379bbc356a3d621
Allexim <drupal@mamatuik.com>
2022-04-16 20:47:24 +0300

    нерабочий вариант. Не сообразил, как передать массив из основного метода во вспомогательный

--- src/ru/eduprof/Main.java
+++ src/ru/eduprof/Main.java
@@ -1,8 +1,65 @@
 package ru.eduprof;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println("The programme of computing of the sum of 3 integer numbers\n");
+
+        // declares an Array of integers.
+        int[] array_new;
+        int num = 3;                //size of array
+
+// allocating memory for 3 integers.
+        array_new = new int[num];
+
+        int sum_of_int;
+
+
+
+
+        // accessing the elements of the specified array
+        for (int i = 0; i < array_new.length; i++) {
+            System.out.print("Input #" + (i+1) + ": ");
+
+            array_new[i] = number_output();
+
+
+        }
+
+        int sum = 0; // initialize sum
+        int k;
+
+        // Iterate through all elements and add them to sum
+        for (k = 0; k < array_new.length; k++)
+            sum += array_new[k];
+
+
+
+        System.out.println("");
+        System.out.println("The sum of numbers = "+sum);
+
+        int summary;
+        summary=Test1.summation();
+        System.out.println(summary);
+
+
+
+
     }
+
+    public static int number_output() {
+        Scanner scanner = new Scanner(System.in);
+        int user_number = scanner.nextInt();
+        return user_number;
+    }
+
+
+
+
+
+
 }
